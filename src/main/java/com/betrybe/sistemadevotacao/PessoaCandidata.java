6eb3256d871977337b5748a30e10d1e64ce6c85e
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-07-10 16:19:31 -0300

    no changes

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,12 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public abstract class PessoaCandidata extends Pessoa {
 
+  protected int numero;
+  protected int votos;
+
+  public PessoaCandidata(int numero, int votos) {
+    this.numero = numero;
+    this.votos = votos;
+  }
 }
